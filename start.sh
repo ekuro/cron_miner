7b53236b4b0827e05c009e30e1bff881bb2dfbf2
ekuro <kurose@bec.co.jp>
2017-12-24 19:38:33 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-echo $(date "+%Y/%m/%d %H:%M:%S")
-/usr/bin/nohup /usr/bin/cpulimit -l 190 -- /usr/local/bin/minerd -a yescrypt -o stratum+tcp://[ip or host]:[port] -u [user] -p [password]
-echo "started!"
+nlines=$(ps aux | grep /usr/local/bin/minerd | wc -l)
+echo $nlines
+if [ $nlines -gt 1 ]; then
+        echo  $(date "+%Y/%m/%d %H:%M:%S") " already started!"
+else
+        /usr/bin/nohup /usr/local/bin/minerd -a yescrypt -o stratum+tcp://[ip or host]:[port] -u [user] -p [password] &
+        echo  $(date "+%Y/%m/%d %H:%M:%S") " started!"
+fi
